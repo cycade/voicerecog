8357c94246a3549032439f2aa44800e5ca5161dc
Inaho <inahocyc@gmail.com>
2020-01-06 15:06:33 +0800

    add mutationObserver to listen change of dom

--- content.js
+++ content.js
@@ -7,4 +7,22 @@
       localStorage.setItem('cursor', e.id);
     }
   });
+
+  const onInputAdded = (mutations) => {
+    mutations.forEach(f => {
+      f.addedNodes.forEach(e => {
+        if (e.tagName === 'INPUT' || e.tagName === 'TEXTAREA') {
+          if (!e.id) { e.id = Math.random().toString(36).slice(-5); }
+          const fn = e.onfocus;
+          e.onfocus = event => {
+            fn && fn(event);
+            localStorage.setItem('cursor', e.id);
+          }
+        }
+      });
+    })
+  }
+
+  let observer = new MutationObserver(onInputAdded);
+  observer.observe(document.querySelector('body'), { childList: true, subtree: true });
 })();
